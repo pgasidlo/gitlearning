c965a8b8fa81cfa02eeda42339332d6eb81aacaf
Piotr Gasidło <quaker+github@barbara.eu.org>
2012-08-26 13:18:05 +0200

    Allow access to secure content by setting cookie.

--- security.php
+++ security.php
@@ -1,5 +1,5 @@
 <?php
 
 function is_secure() {
-  return (PHP_SAPI == 'cli' || @$_SERVER['REMOTE_ADDR'] == '127.0.0.1');
+  return (PHP_SAPI == 'cli' || @$_SERVER['REMOTE_ADDR'] == '127.0.0.1' || @$_COOKIE['debug'] == '0x123456789');
 }
