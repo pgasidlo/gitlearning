bc8f0e01d1623c417e293392e2476760944ce286
Piotr Gasidło <quaker+github@barbara.eu.org>
2012-08-26 13:22:39 +0200

    This will be debug page, not secured one. Added is_debug() function.

---
+++ debug_page.php
@@ -0,0 +1,8 @@
+<?php
+
+include_once "security.php";
+
+if (is_debug()) {
+  print "Debug Content. Do not use on production!";
+}
+
--- secure_page.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-include_once "security.php";
-
-if (is_secure()) {
-  print "Secure Content. Do not copy!";
-}
-
